faf7d70a20bd28c1faa6bd685e17cbf04cc5f53c
Nathan Douglas <github@darkdell.net>
2022-06-03 17:52:52 -0400

    Removed some copypasta I used as a template while writing my matcher.

--- src/rom/header/to_have_valid_header.matcher.ts
+++ src/rom/header/to_have_valid_header.matcher.ts
@@ -16,7 +16,7 @@ const toHaveValidHeader = (received: string) => {
   const pass = validator.validateFile(received);
   if (pass) {
     return {
-      message: () => `expected ${received} not to have a valid header`,
+      message: () => `expected ${received} not to have a valid header (`,
       pass: true,
     };
   } else {
@@ -28,31 +28,3 @@ const toHaveValidHeader = (received: string) => {
 };
 
 export default toHaveValidHeader;
-
-/*
-import { checkHtmlElement, deprecate } from './utils'
-
-export function toBeEmpty(element) {
-  deprecate(
-    'toBeEmpty',
-    'Please use instead toBeEmptyDOMElement for finding empty nodes in the DOM.',
-  )
-  checkHtmlElement(element, toBeEmpty, this)
-
-  return {
-    pass: element.innerHTML === '',
-    message: () => {
-      return [
-        this.utils.matcherHint(
-          `${ this.isNot ? '.not' : '' }.toBeEmpty`,
-          'element',
-          '',
-        ),
-        '',
-        'Received:',
-        `  ${ this.utils.printReceived(element.innerHTML) } `,
-      ].join('\n')
-    },
-  }
-}
-*/
